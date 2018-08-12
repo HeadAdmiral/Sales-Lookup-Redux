0e00e3f6106770c811d0f784e2a584da91d665c1
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-12 14:52:06 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,2 +1,10 @@
-console.log("===== Sales Lookup Redux =====")
-console.log("Extension Loaded."
+window.onload = function(){
+
+    console.log("===== Sales Lookup Redux =====");
+    console.log("Extension Loaded.");
+
+    if (window.location.href.indexOf("Sales") > -1){
+        let tableRow = document.getElementsByTagName("tr");
+        console.log(tableRow);
+    }
+}
