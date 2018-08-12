81b1e4ef8da7be19d5cddadbf5e52ae9b238b29b
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-12 14:37:03 -0500

    Create background.js

---
+++ background.js
@@ -0,0 +1,10 @@
+// Enable extension if user is on the log on/off page in UltiPro
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'orderhistory.microcenter.com/Sales'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
