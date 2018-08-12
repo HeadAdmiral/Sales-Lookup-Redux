623bb56594e6322762aee11617a416e959abc947
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-12 15:00:57 -0500

    Update content.js

--- content.js
+++ content.js
@@ -5,6 +5,17 @@ window.onload = function(){
 
     if (window.location.href.indexOf("Sales") > -1){
         let tableRow = document.getElementsByTagName("tr");
+        let tableData = document.getElementsByTagName("td");
         console.log(tableRow);
+        console.log(tableData);
+        
+        // tableRow[1] should yield tr.k-footer-template which is the row that contains the Customers Served count
+        // The value can be changed under innerHTML
+        
+        // tableData[0] should yield Customers Served: #
+        // The first cell of transactions seems to appear on tableData[20] but further testing is necessary.
+        // Every 8th td is a new line
+        
+        // Just make a function that counts duplicates and you're golden bro
     }
 }
