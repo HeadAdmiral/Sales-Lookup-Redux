a5bf9a97da1207946840b06e14c738344dbace9e
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-12 19:27:15 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,36 @@
+/*45-RE-6934360
+45-PO-6934344
+45-PO-6934344
+45-PO-6934344
+45-PO-6934344
+45-PO-6934344
+45-PO-6934344
+45-PO-6934327
+45-PO-6934319
+45-PO-6934269
+45-PO-6934269
+45-PO-6934269
+45-PO-6934269
+45-PO-6934269
+45-PO-6934269
+45-PO-6934217*/
+
+function getUnique(iterable) {
+    // Counts the number of unique items in the collection provided.
+    // Sets only allow 1 copy of each element in the Set.
+    return new Set(iterable).size;
+}
+
+function getTransactions(tableData){
+    // This function iterates through each row in the Sales Lookup table and adds the transaction ID to an array.
+    let transactions = [];
+    for (let i = 20; i < tableData.length; i+=8){
+        transactions.push(tableData[i]);
+        console.log("Adding transaction " + tableData[i] + " to transactions list.");
+    }
+    return transactions;
+}
+
 window.onload = function(){
 
     console.log("===== Sales Lookup Redux =====");
@@ -6,8 +39,14 @@ window.onload = function(){
     if (window.location.href.indexOf("Sales") > -1){
         let tableRow = document.getElementsByTagName("tr");
         let tableData = document.getElementsByTagName("td");
+        let transactions = getTransactions(tableData);
+        let uniqueTransactions = getUnique(transactions);
+
         console.log(tableRow);
         console.log(tableData);
+        console.log(transactions);
+        console.log(uniqueTransactions);
+
         
         // tableRow[1] should yield tr.k-footer-template which is the row that contains the Customers Served count
         // The value can be changed under innerHTML
@@ -17,5 +56,6 @@ window.onload = function(){
         // Every 8th td is a new line
         
         // Just make a function that counts duplicates and you're golden bro
+
     }
 }
