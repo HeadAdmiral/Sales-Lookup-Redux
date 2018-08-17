822de2c0ecfd244ab78e54d9ea403e6d4d62c560
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-17 18:52:25 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,20 +1,3 @@
-/*45-RE-6934360
-45-PO-6934344
-45-PO-6934344
-45-PO-6934344
-45-PO-6934344
-45-PO-6934344
-45-PO-6934344
-45-PO-6934327
-45-PO-6934319
-45-PO-6934269
-45-PO-6934269
-45-PO-6934269
-45-PO-6934269
-45-PO-6934269
-45-PO-6934269
-45-PO-6934217*/
-
 function getUnique(iterable) {
     // Counts the number of unique items in the collection provided.
     // Sets only allow 1 copy of each element in the Set.
@@ -22,31 +5,42 @@ function getUnique(iterable) {
 }
 
 function getTransactions(tableData){
-    // This function iterates through each row in the Sales Lookup table and adds the transaction ID to an array.
     let transactions = [];
-    for (let i = 20; i < tableData.length; i+=8){
-        transactions.push(tableData[i]);
-        console.log("Adding transaction " + tableData[i] + " to transactions list.");
+    
+    // Adds the Transaction ID from each row in the table to an array
+    for (let i = 0; i < tableData.rows.length; i++){
+        transactions.push(tableData.rows[i].cells[0].innerText);
     }
+   
     return transactions;
 }
 
 window.onload = function(){
 
-    console.log("===== Sales Lookup Redux =====");
-    console.log("Extension Loaded.");
-
+    // If the current page is Sales Lookup...
     if (window.location.href.indexOf("Sales") > -1){
-        let tableRow = document.getElementsByTagName("tr");
-        let tableData = document.getElementsByTagName("td");
-        let transactions = getTransactions(tableData);
+        console.log("===== Sales Lookup Redux =====");
+        console.log("Extension Loaded.");
+        
+        let statsTable = document.getElementsByTagName("tbody")[0];
+        let salesTable = document.getElementsByTagName("tbody")[1];
+        let customersServed = document.getElementsByTagName("td")[0];
+        
+                
+        let transactions = getTransactions(salesTable);
         let uniqueTransactions = getUnique(transactions);
+        customersServed.innerText = "Customers Served: " + uniqueTransactions;	
+        
 
-        console.log(tableRow);
-        console.log(tableData);
-        console.log(transactions);
+        
         console.log(uniqueTransactions);
+        console.log(document.getElementsByTagName("td")[0]);
 
+
+        /* This works correctly, but it counts returns/exchanges as sales. You should count sales only by checking the column
+         * with sales in it and having those separate from returns and exchanges.
+         */
+        
         
         // tableRow[1] should yield tr.k-footer-template which is the row that contains the Customers Served count
         // The value can be changed under innerHTML
