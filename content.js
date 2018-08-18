51444350ffcfc7d5aaa0565c198a90f8d2819135
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-18 18:40:38 -0500

    Update content.js

--- content.js
+++ content.js
@@ -4,15 +4,30 @@ function getUnique(iterable) {
     return new Set(iterable).size;
 }
 
-function getTransactions(tableData){
+function getTransactions(tableData, transactionType){
     let transactions = [];
     
     // Adds the Transaction ID from each row in the table to an array
     for (let i = 0; i < tableData.rows.length; i++){
-        transactions.push(tableData.rows[i].cells[0].innerText);
+        if (tableData.rows[i].cells[1].innerText === transactionType){
+            transactions.push(tableData.rows[i].cells[0].innerText);
+        }
     }
    
     return transactions;
+}
+
+function createRow(table, rowHeading, quantity){
+    let row = table.insertRow(table.rows.length);
+    row.className = "k-footer-template";
+    
+    for (let i = 0; i < 6; i++){
+        let cell = row.insertCell(i);
+        
+        if (i == 0){
+            cell.innerHTML = (rowHeading + " " + quantity).bold();
+        }
+    }
 }
 
 window.onload = function(){
@@ -27,11 +42,18 @@ window.onload = function(){
         let customersServed = document.getElementsByTagName("td")[0];
         
                 
-        let transactions = getTransactions(salesTable);
+        let transactions = getTransactions(salesTable, "Sale");
+        let returns = getTransactions(salesTable, "Return");
+        let exchanges = getTransactions(salesTable, "Exchange");
         let uniqueTransactions = getUnique(transactions);
-        customersServed.innerText = "Customers Served: " + uniqueTransactions;	
+        let uniqueReturns = getUnique(returns);
+        let uniqueExchanges = getUnique(exchanges);
         
-
+        customersServed.innerText = "Customers Served: " + uniqueTransactions;	
+       
+        createRow(statsTable, "Returns:", uniqueReturns);
+        createRow(statsTable, "Exchanges:", uniqueExchanges);
+        createRow(statsTable, "", "");
         
         console.log(uniqueTransactions);
         console.log(document.getElementsByTagName("td")[0]);
